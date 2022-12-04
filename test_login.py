3d9ba2d6174eefc880627dc226b34958dd5eca94
alvy-tiket <muhammad.fauzi@tiket.com>
2022-12-04 14:51:16 +0700

    feat: add login with random user

--- test_login.py
+++ test_login.py
@@ -1,8 +1,11 @@
+import random
+import string
+from time import sleep
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 
 
 @pytest.fixture()
@@ -28,3 +31,14 @@ def test_login_logout(driver):
     header_login = driver.find_element(
         By.XPATH, '//a[contains(text(),"Log in")]')
     assert header_login is not None
+
+
+def test_login_with_random_account(driver):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    randomString = ''.join(random.choice(characters) for i in range(8))
+    driver.find_element(
+        By.ID, 'username').send_keys(randomString)
+    driver.find_element(
+        By.ID, 'password').send_keys(randomString + Keys.ENTER)
+    alert = driver.find_element(By.CLASS_NAME, 'alert')
+    assert alert is not None
