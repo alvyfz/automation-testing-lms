4b4b6bf62b0049717af95327a5ead272e05fa1aa
alvy-tiket <muhammad.fauzi@tiket.com>
2022-12-19 10:36:12 +0700

    fix: test course announcement

---
+++ test_course.py
@@ -0,0 +1,43 @@
+import random
+import string
+from time import sleep
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.get('https://elearning.ibik.ac.id/login/index.php')
+    yield driver
+    driver.quit()
+
+
+def test_course_announcement(driver):
+    driver.find_element(
+        By.ID, 'username').send_keys('192310009')
+    driver.find_element(
+        By.ID, 'password').send_keys('Fauzi29030!' + Keys.ENTER)
+    assert "Elearning" in driver.title
+    banner = driver.find_element(
+        By.XPATH, '//div/img')
+    assert banner is not None
+    driver.find_element(By.ID, 'action-menu-toggle-1').click()
+    sleep(1)
+    driver.find_element(
+        By.XPATH, '//a[@data-title="mymoodle,admin"]').click()
+    sleep(3)
+    course_card = driver.find_element(By.CLASS_NAME, "card")
+    assert course_card is not None
+    clickable_card_course = driver.find_element(
+        By.XPATH, ' (//a[@class ="aalink coursename mr-2"])[1]')
+    clickable_card_course.click()
+    driver.find_element(
+        By.XPATH, '(//a[@class="aalink"])[1]').click()
+    announcement = driver.find_element(
+        By.XPATH, '//h2[contains(text(),"Announcements")]')
+    assert announcement is not None
+    sleep(5)
